6a6d4f150784b998474ee250c0540bcd996ef544
Fardin2112 <93831526+Fardin2112@users.noreply.github.com>
2022-09-23 23:32:16 +0530

    Update Basics.cpp

--- Basics.cpp
+++ Basics.cpp
@@ -199,3 +199,41 @@ int main(){
 
     return 0;
 }
+
+
+Q10 : binary search in array :
+#include<iostream>
+using namespace std;
+int binary_search(int arr[],int n,int key){
+int s=0;//starting point
+int e=n;//e for end number
+while(s<=e){
+    int mid=(s=n)/2;
+    if( mid==key){
+        return mid;
+    } else if(mid>key){
+        mid=e-1;
+    }
+    else{
+        mid=s+1;
+    }
+}
+return -1;
+}
+    
+int main(){
+    int n;
+    cout<<"write no n:"<<endl;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"write value of arr at :"<<i<<endl;
+        cin>>arr[i];
+    }
+    int key;
+    cout<<"key is :"<<endl;
+    cin>>key;
+    cout<<"element "<<binary_search( arr, n, key)<<endl;
+
+    return 0;
+}
