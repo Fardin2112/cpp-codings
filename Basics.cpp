e5fd382b9433e0a6b48d11aa90ea8169cdb84f79
Fardin2112 <93831526+Fardin2112@users.noreply.github.com>
2022-09-23 19:51:17 +0530

    Update Basics.cpp

--- Basics.cpp
+++ Basics.cpp
@@ -170,3 +170,32 @@ for(int i=0;i<n;i++){
 cout<<"max no:"<<maxno<<" "<<"min no: "<<minno;
 	return 0;
 }
+
+Q9 finding key element in array:
+
+ #include<iostream>
+using namespace std;
+int lenear_search(int arr[],int n,int key){
+    for(int i=0;i<n;i++){
+        if(arr[i]==key){
+            return i;
+        }
+    }
+    return -1;
+}
+int main(){
+    int n;
+    cout<<"write no n:"<<endl;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"write value of arr at :"<<i<<endl;
+        cin>>arr[i];
+    }
+    int key;
+    cout<<"key is :"<<endl;
+    cin>>key;
+    cout<<"element "<<lenear_search( arr, n, key)<<endl;
+
+    return 0;
+}
